e90b079798a05e9f18ec0611e5704ba0e01c655a
Hieu Nguyen <hnnguyen45@gmail.com>
2016-12-11 23:23:11 -0800

    started on HTML and added a few others in the other folders.

---
+++ test/test-server.js
@@ -0,0 +1,13 @@
+//Created by Hieu Nguyen
+global.DATABASE_URL = 'mongodb://localhost/budget';
+
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+
+var server = require('../server.js');
+var Item = require('../models/item');
+
+var should = chai.should();
+var app = server.app;
+
+chai.use(chaiHttp);
